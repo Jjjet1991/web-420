4c649a00fdc7e93a4bead2550a9ce73cb4e6ea98
Jourdan Neal <jourdan.a.neal@gmail.com>
2021-06-27 19:26:27 -0700

    Add files via upload

---
+++ neal-composer.js
@@ -0,0 +1,26 @@
+/*
+=====================================================
+; Title: Assignment 4.2 Composer API
+; Author: Professor Krasso
+; Date 20 June 2021
+; Modified By: Jourdan Neal
+; Description: Create composer API connected with MongooseDB Atlas.
+=====================================================
+*/
+
+//Add required libraries
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//Create Schema
+var composerSchema = new Schema({
+    //Name fields
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true}
+});
+
+//Create Composer model
+var Composer = mongoose.model('Composer', composerSchema);
+
+//Export Composer model
+module.exports = Composer; 
